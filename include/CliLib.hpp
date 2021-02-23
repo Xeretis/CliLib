f32ed72f334fe6f8866011a7e035cedf480966e8
Xeretis <70800211+Xeretis@users.noreply.github.com>
2021-02-23 21:42:31 +0100

    Got started with those alternative ways to use OptionGroups

--- include/CliLib.hpp
+++ include/CliLib.hpp
@@ -12,15 +12,27 @@ enum class Policy {
     ONEOF
 };
 
+struct Option {
+    Option(std::string opt, std::string desc, std::string longOption = "");
+
+    std::string opt;
+    std::string desc;
+    std::string longOption;
+};
+
+
 class OptionGroup {
 public:
     OptionGroup(Policy p, std::string description);
 
-    void addOption(const std::string& option, const std::string& desc, const std::string& longOption="");
+    void addOption(const std::string& opt, const std::string& desc, const std::string& longOption="");
+    void addOption(Option* newOption);
+
+    ~OptionGroup();
 
     Policy policy;
     std::string groupDescription;
-    std::map<std::string, std::pair<std::string, std::string>> options;
+    std::vector<Option*> options;
 };
 
 class Command {
@@ -30,7 +42,7 @@ public:
 
     void setAsDefault();
     void addSubCommand(const std::string& name, Command* newSubCommand);
-    void addOptionGroup(const OptionGroup& group);
+    void addOptionGroup(OptionGroup* group);
     void setNoReaminder(bool newNoRemainder);
 
     void run(std::vector<std::string>& rawArgs) const;
@@ -40,7 +52,7 @@ public:
 
 private:
     std::map<std::string, Command*> subCommands;
-    std::vector<OptionGroup> optionGroups;
+    std::vector<OptionGroup*> optionGroups;
     std::function<void()> callback;
     std::string description;
     bool noRemainder = true;
@@ -61,7 +73,6 @@ public:
     static bool isSet(const std::string &option);
     static bool hasOptionSyntax(const std::string& str);
 
-    static bool splitFlags;
     static std::vector<std::string> tokens;
     static Command* defaultCommand;
 private:
@@ -70,11 +81,25 @@ private:
     static std::vector<std::string> getMultiRaw(const std::string& option, const std::string& longOption = "");
 };
 
+//Option
+
+Option::Option(std::string opt, std::string desc, std::string longOption) : opt(std::move(opt)), desc(std::move(desc)), longOption(std::move(longOption)) { }
+
 //OptionGroup
 OptionGroup::OptionGroup(Policy p, std::string description) : policy(p), groupDescription(std::move(description)) {}
 
-void OptionGroup::addOption(const std::string &option, const std::string &desc, const std::string &longOption) {
-    options[option] = std::make_pair(desc, longOption);
+void OptionGroup::addOption(const std::string &opt, const std::string &desc, const std::string &longOption) {
+    options.emplace_back(new Option(opt, desc, longOption));
+}
+
+void OptionGroup::addOption(Option *newOption) {
+    options.emplace_back(newOption);
+}
+
+OptionGroup::~OptionGroup() {
+    for (Option* option : options) {
+        delete option;
+    }
 }
 
 //Command
@@ -89,7 +114,7 @@ void Command::addSubCommand(const std::string& name, Command* newSubCommand) {
     subCommands.emplace(name, newSubCommand);
 }
 
-void Command::addOptionGroup(const OptionGroup& group) {
+void Command::addOptionGroup(OptionGroup* group) {
     optionGroups.push_back(group);
 }
 
@@ -135,15 +160,15 @@ bool Command::validateOptions() const {
 
     for (const auto& group : optionGroups) {
         bool wasOne = false;
-        for (const auto& option : group.options) {
-            valid = Parser::isSet(option.first) || Parser::isSet(option.second.second);
-            if ((group.policy == Policy::REQUIRED && !valid) || (group.policy == Policy::ANYOF && valid))
+        for (const auto& option : group->options) {
+            valid = Parser::isSet(option->opt) || Parser::isSet(option->longOption);
+            if ((group->policy == Policy::REQUIRED && !valid) || (group->policy == Policy::ANYOF && valid))
                 break;
-            else if (group.policy == Policy::ONEOF && valid && !wasOne)
+            else if (group->policy == Policy::ONEOF && valid && !wasOne)
                 wasOne = true;
-            else if (group.policy == Policy::ONEOF && valid && wasOne)
+            else if (group->policy == Policy::ONEOF && valid && wasOne)
                 valid = false;
-            else if ((group.policy == Policy::OPTIONAL) || (group.policy == Policy::ONEOF && !valid && wasOne))
+            else if ((group->policy == Policy::OPTIONAL) || (group->policy == Policy::ONEOF && !valid && wasOne))
                 valid = true;
         }
         if (!valid)
@@ -177,9 +202,9 @@ void Command::printHelp(const std::string &title) const {
         std::cout << "Options:";
 
         for (const auto& group : optionGroups) {
-            std::cout << "\n[" + group.groupDescription + "]\n";
-            for (auto& option : group.options)
-                std::cout << "\t" << option.first << (option.second.second.empty() ? "" : ", " + option.second.second) << " - " << option.second.first << std::endl;
+            std::cout << "\n[" + group->groupDescription + "]\n";
+            for (auto& option : group->options)
+                std::cout << "\t" << option->opt << (option->longOption.empty() ? "" : ", " + option->longOption) << " - " << option->desc << std::endl;
         }
     }
 }
@@ -190,28 +215,26 @@ const std::string &Command::getDescription() const {
 
 bool Command::isOption(const std::string &str) const {
     for (const auto& group : optionGroups) {
-        for (const auto& o : group.options)
-            if (str==o.first || str==o.second.second) return true;
+        for (const auto& option : group->options)
+            if (str == option->opt || str == option->longOption) return true;
     }
     return false;
 }
 
 //Parser
 void Parser::parse(const int& argc, const char* const* argv, bool splitFlags) {
-    Parser::splitFlags = splitFlags;
-
     for (int i = 1; i < argc; ++i) {
         std::string current = argv[i];
         if (splitFlags && std::regex_match(current, std::regex("^(-[a-zA-Z]{2,})(=.*$|$)"))) {
             for (int j = 1; j < current.size() && current[j - 1] != '='; ++j)
-                tokens.push_back((current[j] != '=') ? std::string{'-', current[j]} : current.substr(j + 1));
+                tokens.emplace_back((current[j] != '=') ? std::string{'-', current[j]} : current.substr(j + 1));
         } else {
             size_t equal_pos = current.find_first_of('=');
             if (equal_pos == std::string::npos)
-                tokens.push_back(current);
+                tokens.emplace_back(current);
             else {
-                tokens.push_back(current.substr(0, equal_pos));
-                tokens.push_back(current.substr(equal_pos + 1));
+                tokens.emplace_back(current.substr(0, equal_pos));
+                tokens.emplace_back(current.substr(equal_pos + 1));
             }
         }
     }
@@ -365,7 +388,6 @@ bool Parser::hasOptionSyntax(const std::string& str) {
     return std::regex_match(str, std::regex("^(-{1,2}[a-zA-Z0-9_]{1,})"));
 }
 
-bool Parser::splitFlags;
 std::vector<std::string> Parser::tokens;
 Command* Parser::defaultCommand;
 
